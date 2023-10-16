98eec69a19e789c616b112ff02fcb2c420cb0313
徐执默 <45121260+amluckydave@users.noreply.github.com>
2023-10-16 23:21:54 +0800

    11-watch

---
+++ vue3-basics/src/views/11-watcher/Demo.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref, watchEffect, watch, computed } from 'vue';
+
+const counter = ref(1);
+const doubleCounter = computed(() => counter.value * 2);
+
+const handleClick = () => {
+    counter.value++;
+};
+
+// watchEffect(() => {
+//     console.log('counter has been changed to ' + counter.value);
+// });
+
+watch(
+    () => counter.value,
+    (newValue, oldValue) => {
+        console.log('counter has been from ' + oldValue + ' changed to ' + newValue);
+    }
+);
+</script>
+
+<template>
+    <div>
+        <div>
+            {{ counter }}
+            {{ doubleCounter }}
+        </div>
+        <div>
+            <button @click="handleClick">Increment</button>
+        </div>
+    </div>
+</template>
+
+<style lang="css">
+</style>
